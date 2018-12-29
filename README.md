38d6efedd771733d0820f25617ac45795d558dd1
leo <leo@leopard.lan>
2018-12-30 00:41:51 +0800

    first import

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# cnotes
-command line notes utils
+# Cnotes
+
+Command line notes utility written in Crystal programming language.
+
+# Installation
+
+copy notes to \$PATH
+
+# Usage
+
+```shell
+  notes - notes utility save data in local sqlite3 database.
+
+  Usage:
+    notes [command] [arguments]
+
+  Commands:
+    a               # add note.
+    d               # delete note by id.
+    help [command]  # Help about any command.
+    init            # init sqlite3 db.
+    l               # list all notes.
+    s               # search notes by keywords.
+```
+
+Data file path : ~/.note.sqlite3
+
+# Build
+
+```shell
+git clone https://github.com/hjleochen/cnotes.git
+shards
+crystal build notes.cr --release
+```
---
+++ notes.cr
@@ -0,0 +1,116 @@
+require "commander"
+require "sqlite3"
+
+class Cnote
+	FILE_NAME = File.expand_path("~/.note.sqlite3")
+  DB_NAME = "sqlite3://#{FILE_NAME}"
+
+	def init
+		unless File.exists? FILE_NAME
+			DB.open DB_NAME do |db|
+				db.exec "create table notes (content text)"
+			end
+		end
+
+		return "init db #{FILE_NAME}"
+  end
+	
+	def add(content)
+		DB.open DB_NAME do |db|
+			db.exec "insert into notes(content) values (?)", content
+		end
+	end
+  
+	def delete(rowid)
+		DB.open DB_NAME do |db|
+			db.exec "delete from notes where rowid = ?", rowid
+		end
+  end
+
+	def list
+		find_by_sql("select rowid,content from notes order by rowid")
+  end
+
+	def search(key_words)
+		find_by_sql("select rowid,content from notes where content like ? order by rowid","%"+key_words+"%")
+	end
+
+	def find_by_sql(sql, *args)
+		DB.open DB_NAME do |db|
+			db.query(sql, *args) do |rs|
+				rs.each do
+					puts "\n" + "-" * 10 + "#{rs.read(Int32)}" + "-" * 10
+					puts rs.read(String)
+				end
+			end
+		end
+	end
+end
+
+
+cnote = Cnote.new
+cnote.init
+
+cli = Commander::Command.new do |cmd|
+  cmd.use = "notes"
+  cmd.long = "notes utility save data in local sqlite3 database."
+
+  cmd.run do |options, arguments|
+    puts cmd.help
+  end
+
+	cmd.commands.add do |cmd|
+    cmd.use = "init"
+    cmd.short = "init sqlite3 db."
+    cmd.long = cmd.short
+    cmd.run do |options, arguments|
+			puts cnote.init
+    end
+	end
+
+	cmd.commands.add do |cmd|
+    cmd.use = "a"
+    cmd.short = "add note."
+    cmd.long = cmd.short
+    cmd.run do |options, arguments|
+			cnote.add arguments.join(" ")
+    end
+	end
+
+	cmd.commands.add do |cmd|
+    cmd.use = "l"
+    cmd.short = "list all notes."
+    cmd.long = cmd.short
+    cmd.run do |options, arguments|
+			cnote.list
+    end
+	end
+
+	cmd.commands.add do |cmd|
+    cmd.use = "d"
+    cmd.short = "delete note by id."
+    cmd.long = cmd.short
+    cmd.run do |options, arguments|
+			if arguments.size == 0
+				puts cmd.help	
+			else
+				cnote.delete arguments[0]
+			end
+    end
+	end
+
+	cmd.commands.add do |cmd|
+    cmd.use = "s"
+    cmd.short = "search notes by keywords."
+    cmd.long = cmd.short
+    cmd.run do |options, arguments|
+			if arguments.size == 0
+				puts cmd.help	
+			else
+				cnote.search arguments.join(" ")
+			end
+    end
+	end
+end
+
+Commander.run(cli, ARGV)
